a39886c457d29e4a9ab4d82f3723960ba3eae703
demon28 <274733956@qq.com>
2018-07-05 13:38:14 +0800

    未加课

---
+++ Remover.Robot/Form2.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace Remover.Robot
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tb_cishu = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tb_jiange = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.tb_gedu = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(214, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "奥特曼-注入";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Location = new System.Drawing.Point(2, 121);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1214, 525);
+            this.panel1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(214, 40);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(123, 30);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "奥特曼-开始";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "次数：";
+            // 
+            // tb_cishu
+            // 
+            this.tb_cishu.Location = new System.Drawing.Point(63, 9);
+            this.tb_cishu.Name = "tb_cishu";
+            this.tb_cishu.Size = new System.Drawing.Size(72, 21);
+            this.tb_cishu.TabIndex = 4;
+            this.tb_cishu.Text = "300";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "间隔：";
+            // 
+            // tb_jiange
+            // 
+            this.tb_jiange.Location = new System.Drawing.Point(63, 43);
+            this.tb_jiange.Name = "tb_jiange";
+            this.tb_jiange.Size = new System.Drawing.Size(73, 21);
+            this.tb_jiange.TabIndex = 6;
+            this.tb_jiange.Text = "3";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(214, 73);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(123, 30);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "奥特曼-停止";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // tb_gedu
+            // 
+            this.tb_gedu.Location = new System.Drawing.Point(63, 73);
+            this.tb_gedu.Name = "tb_gedu";
+            this.tb_gedu.Size = new System.Drawing.Size(73, 21);
+            this.tb_gedu.TabIndex = 9;
+            this.tb_gedu.Text = "3";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "额度：";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(142, 46);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 12);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "(秒)";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(368, 14);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 12);
+            this.label5.TabIndex = 11;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1220, 658);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tb_gedu);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.tb_jiange);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tb_cishu);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form2";
+            this.Text = "Robort";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tb_cishu;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tb_jiange;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox tb_gedu;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label5;
+    }
+}
---
+++ Remover.Robot/Form2.cs
@@ -0,0 +1,109 @@
+﻿using CefSharp;
+using CefSharp.WinForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Remover.Robot
+{
+    public partial class Form2 : Form
+    {
+
+        public ChromiumWebBrowser webBrowser;
+
+        public Form2()
+        {
+            InitializeComponent();
+
+            var setting = new CefSettings
+            {
+
+                Locale = "zh-CN",
+                AcceptLanguageList = "zh-CN",
+                MultiThreadedMessageLoop = true
+
+            };
+
+            Cef.Initialize(setting);
+            webBrowser = new ChromiumWebBrowser("https://www.coineal.com/index.html#zh_CN");
+
+            this.panel1.Controls.Add(webBrowser);
+            webBrowser.Dock = DockStyle.Fill;
+            
+           
+        }
+
+        string script = @"var body=$(""body"");body.append('<div style=""position: absolute;top: 0;left: 0;z-index: 9999999;""><input type="""" style=""padding: 0;margin: 0;display:  block;width: 100px;height: 30px;float: left;""  name=""""  visible=""falase"" id=""buyamount""><button style=""width: 30px;height: 30px;border: none;background: green;padding: 0;margin: 0;display: block;float: left;""   onclick=""mybuybtn()"" id=""buybutton"">买</button><button style=""width: 30px;height: 30px;border: none;padding: 0;margin: 0;background: green;display: block;float: left;"" s onclick=""mysellbtn()""  id=""sellbutton"">卖</button><span style=""clear: both;""></span></div>');function mybuybtn(){$("".getCountCoinSJ"").val($(""#buyamount"").val());$("".transaction-buy-new"").eq(1).find("".first_tab_button"").click()}function mysellbtn(){$("".transaction-sell-new"").eq(1).find("".first_tab_button"").click()}setInterval(function(){var datad=$("".userOder-list tr.list-con"");for(var i=0;i<datad.length;i++){var tardom=datad[i];var tt=new Date($(tardom).html().substring(0,23).substring(4,100));var nnow=new Date();if(nnow.getTime()-tt.getTime()>30000){$("".cancel_Deal"",tardom).click()}}},60000);setInterval(function(){var tvalue=$("".baseCoin s"").html().split("" "")[0];var num=tvalue.indexOf(""."");if(num>0){tvalue=tvalue.substring(0,num+3)}$(""#getBaseCoin"").val(tvalue)},300)";
+
+
+        int count = 0;
+        int jiange = 0;
+        decimal jine = 0;
+        int cishu = 0;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var browser = webBrowser.GetBrowser().MainFrame;
+     
+            webBrowser.GetBrowser().MainFrame.ExecuteJavaScriptAsync(script);
+
+
+            int.TryParse(tb_jiange.Text, out jiange);
+
+            decimal.TryParse(tb_gedu.Text, out jine);
+
+            int.TryParse(tb_cishu.Text, out cishu);
+
+            string setvalue = @"$(""#buyamount"").val('" + jine + "')";
+
+            webBrowser.GetBrowser().MainFrame.ExecuteJavaScriptAsync(setvalue);
+        }
+
+    
+        private void button2_Click(object sender, EventArgs e)
+        {
+         
+            this.timer1.Interval = jiange * 1000;
+            this.timer1.Tick += Timer1_Tick;
+            this.timer1.Start();
+        }
+
+        bool falg = true;
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            count = count + 1;
+            this.label5.Text = count.ToString();
+
+            if (count >= cishu)
+            {
+                this.timer1.Stop();
+            }
+
+            if (falg)
+            {
+                webBrowser.GetBrowser().MainFrame.ExecuteJavaScriptAsync("mybuybtn();");
+                falg = false;
+            }
+            else
+            {
+                webBrowser.GetBrowser().MainFrame.ExecuteJavaScriptAsync("mysellbtn();");
+                falg = true;
+            }
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            count=0;
+            this.timer1.Stop();
+        }
+    }
+}
